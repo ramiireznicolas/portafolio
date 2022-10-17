17010035e2ad62458ced36d6cc91134774fa610a
Nicolas Ramirez <nicolasramirez22498@gmail.com>
2022-10-17 13:53:37 -0300

    se agrego animaciones en js

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ navWrapper.addEventListener("click", e => {
     toggleButton.classList.remove("close");
   }
 });
+
+//?SCROOL WINDOWS
+window.addEventListener('scroll', function()  {
+  let elements = document.getElementsByClassName('scroll-content');
+  let screenSize = window.innerHeight;
+  
+    for(var i = 0; i < elements.length; i++) {
+      let element = elements[i];
+
+      if(element.getBoundingClientRect().top < screenSize) {
+        element.classList.add('visible');
+      } else {
+        element.classList.remove('visible');
+      }
+    }
+});
